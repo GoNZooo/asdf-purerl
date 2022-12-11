126c1003c283035182772527ac4dbcb69e2e17f7
Rickard Andersson <gonz@severnatazvezda.com>
2022-12-11 11:33:38 +0200

    make updates to utils corresponding to `purerl`

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for purerl.
 GH_REPO="https://github.com/purerl/purerl"
 TOOL_NAME="purerl"
 TOOL_TEST="purerl --version"
@@ -31,8 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if purerl has other means of determining installable versions.
   list_github_tags
 }
 
@@ -41,8 +38,16 @@ download_release() {
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for purerl
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    archive="linux64.tar.gz"
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    archive="macos.tar.gz"
+  else
+    fail "Unsupported OS: $OSTYPE"
+  fi
+
+  url="$GH_REPO/releases/download/v${version}/${archive}"
+
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -61,7 +66,6 @@ install_version() {
     mkdir -p "$install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-    # TODO: Assert purerl executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
